17598514decd9213dacb67ea7ddc8e4dee9132a3
baurlers <baurlers@student.ethz.ch>
2025-03-13 19:03:36 +0100

    monknemove

--- src/ObjectClasses/Player.cs
+++ src/ObjectClasses/Player.cs
@@ -1,3 +1,4 @@
+using Microsoft.Xna.Framework.Input;
 
 using System.Numerics;
 using System.Runtime.CompilerServices;
@@ -7,14 +8,29 @@ namespace src.ObjectClasses
 /** Anything regarding player characters, from movement to the actual Model files goes here. **/
     public class Player : MoveableObject {
 
-        private Vector3 position;
+        private Vector3 position = new Vector3(0,-3f,0);
+        private float movespeed=0.2f;
         private int life = 3; 
         private int stamina = 3; 
-        private Projectile projectileHeld; 
+        //private Projectile *projectileHeld;
         private bool isHoldingProjectile = false;
-
+        public Vector3 getposition(){
+            return this.position;
+        }
         public void Move(){
-
+            KeyboardState newState = Keyboard.GetState();
+                if(newState.IsKeyDown(Keys.A)){
+                    position.X += movespeed;
+                }
+                if(newState.IsKeyDown(Keys.W)){
+                    position.Y -=movespeed;
+                }
+                if(newState.IsKeyDown(Keys.S)){
+                    position.Y +=movespeed;
+                }
+                if(newState.IsKeyDown(Keys.D)){
+                    position.X-=movespeed;
+                }
         }
 
         public void Grab(){
@@ -28,7 +44,7 @@ namespace src.ObjectClasses
         public void Throw(){
             //if(button pressed)
             //looking direction
-            projectileHeld.Throw(position, new Vector3(0, 0, 0));
+            //projectileHeld.Throw(position, new Vector3(0, 0, 0));
             this.isHoldingProjectile = false;
         }
 
