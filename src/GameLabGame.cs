9f025b8102637aad98d0417b8b07f6d54f4304e3
Edoardo Negri <enegri@ethz.ch>
2025-03-14 10:22:18 +0100

    yes

--- src/GameLabGame.cs
+++ src/GameLabGame.cs
@@ -21,9 +21,9 @@ namespace GameLab
         private SpriteBatch _spriteBatch;
 
         private Model plane, jesterModel, arena;
-        private Dictionary<int, Model> projectileModels;
+        private List<Model> prjModels = new List<Model>();
         private RingOfDoom ring;
-        private LinkedList<Projectile> projectiles = new LinkedList<Projectile>();
+        private LinkedList<Projectile> activePrj = new LinkedList<Projectile>();
         private Player[] players = new Player[4];
         private Matrix view = Matrix.CreateLookAt(new Vector3(0f, 5.0f, 5.0f), new Vector3(0, 0, 0), Vector3.UnitZ);
         private Matrix projection = Matrix.CreateOrthographic(15, 15, 0.1f, 1000f);
@@ -62,12 +62,9 @@ namespace GameLab
             plane = Content.Load<Model>("BIG");
             arena = Content.Load<Model>("arena");
             jesterModel = Content.Load<Model>("Jester");
-
-            projectileModels = new Dictionary<int, Model>
-            {
-                { 0, Content.Load<Model>("Frog") }, // Projectile type 0: Frog
-                { 1, Content.Load<Model>("Monke") }, // Projectile type 1: Swordfish
-            };
+            //Add projectile models
+            prjModels.Add(Content.Load<Model>("Frog"));
+            prjModels.Add(Content.Load<Model>("Monke"));
         }
 
         protected override void Update(GameTime gameTime)
@@ -80,12 +77,12 @@ namespace GameLab
             if (timeUntilNextProjectile <= 0)
             {
                 timeUntilNextProjectile = (float)rng.NextDouble() * 5000f;
-                int type = rng.Next(0, projectileModels.Count);
-                projectiles.AddLast(Projectile.CreatePrj(type, ring.RndCircPoint(), players[rng.Next(0, 4)].GetPosition()));
+                int type = rng.Next(0, prjModels.Count);
+                activePrj.AddLast(Projectile.CreatePrj(type, ring.RndCircPoint(), players[rng.Next(0, 4)].GetPosition()));
             }
 
             //move all the projectiles
-            foreach (Projectile projectile in projectiles) projectile.Move(gameTime);
+            foreach (Projectile projectile in activePrj) projectile.Move(gameTime);
 
             //move players
             foreach (Player player in players) player.Move(gameTime);
@@ -128,8 +125,8 @@ namespace GameLab
 
             DrawModel(plane, planeTran);
 
-            foreach (Projectile projectile in projectiles)
-                DrawModel(projectileModels[projectile.GetTipe()], Matrix.CreateTranslation(projectile.GetPosition()));
+            foreach (Projectile projectile in activePrj)
+                DrawModel(prjModels[projectile.GetTipe()], Matrix.CreateTranslation(projectile.GetPosition()));
 
             foreach (Player player in players)
                 DrawModel(jesterModel, Matrix.CreateTranslation(player.GetPosition()));
