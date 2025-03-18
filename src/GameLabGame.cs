b9d63553fa965d67f3ef36765138c03a4f59c198
Albert Sandru <asandru@studnet.ethz.ch>
2025-03-18 18:01:35 +0100

    Resolved conflicts in Player, Projectile and GameLab classes

--- src/GameLabGame.cs
+++ src/GameLabGame.cs
@@ -30,10 +30,12 @@ namespace GameLab
         private Dictionary<ProjectileType, Model> projectileModels = new Dictionary<ProjectileType, Model>();
         private RingOfDoom ring;
         private LinkedList<Projectile> activeProjectiles = new LinkedList<Projectile>();
+        private LinkedList<Projectile> hitProjectiles = new LinkedList<Projectile>();
 
         // Player settings
-        private static int NUM_PLAYERS = 1;
+        private static int NUM_PLAYERS = 2;
         private Player[] players = new Player[NUM_PLAYERS];
+        private Vector3 playerSpawnOrientation = new Vector3(0,0,-1);
 
         // Camera settings
         private Matrix view = Matrix.CreateLookAt(new Vector3(0f, 10, 7), new Vector3(0, 0, 0), Vector3.Up);
@@ -63,22 +65,41 @@ namespace GameLab
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
         }
-
+        private void initializePlayers()
+        {
+            int n = players.Length;
+            float[] playerStartPositions = { -0.75f, -0.25f, 0.25f, 0.75f };
+            switch (n)
+            {
+                case 4:
+                    players[3] = new Player(new Vector3(playerStartPositions[3], 0, 0),new InputController(PlayerIndex.Three));
+                    goto case 3;
+                case 3:
+                    players[2] = new Player(new Vector3(playerStartPositions[2], 0, 0),new InputController(PlayerIndex.Two));
+                    goto case 2;
+                case 2:
+                    //players[1] = new Player(new Vector3(playerStartPositions[1], 0, 0),new InputController(PlayerIndex.One));
+                    players[1] = new Player(new Vector3(playerStartPositions[1], 0, 0),new InputKeyboard());
+                    goto case 1;
+                case 1: 
+                    players[0] = new Player(new Vector3(playerStartPositions[0], 0, 0),new Input());
+                    goto default;
+                default:
+                break;
+            }
+        }
         protected override void Initialize()
         {
             _graphics.PreferredBackBufferWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width;
             _graphics.PreferredBackBufferHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
-            _graphics.IsFullScreen = true; // Enable full screen
+            _graphics.IsFullScreen = false; // Enable full screen
             _graphics.ApplyChanges();
 
             // Initialize the ring of doom:
             int planeWidth = 10, planeHeight = 10;
             this.ring = new RingOfDoom(planeWidth, planeHeight);
-
-            float[] playerStartPositions = { 0,0,0,0 };
-            for (int i = 0; i < NUM_PLAYERS; i++)
-                players[i] = new Player(new Vector3(playerStartPositions[i], 0, 0));
-
+                
+            initializePlayers();
             base.Initialize();
         }
 
@@ -135,13 +156,12 @@ namespace GameLab
             }
 
             // Move all the projectiles
-            foreach (Projectile projectile in activeProjectiles) projectile.Move(dt, players[0].Position);
+            foreach (Projectile projectile in activeProjectiles) projectile.Update(dt);
 
             // Move players
             foreach (Player player in players)
             {
-                player.Move(dt);
-                player.Throw(activeProjectiles);
+                player.update(dt);
             }
 
 
@@ -151,11 +171,14 @@ namespace GameLab
                 foreach (Projectile projectile in activeProjectiles)
                 {
                     //we should decide how much distance
-                    if (player.Grab(projectile, activeProjectiles) || player.GetHit(projectile, activeProjectiles, players))
-                        break;
+                    if (player.GrabOrHit(projectile, activeProjectiles))
+                        hitProjectiles.AddLast(projectile);
+                        //activeProjectiles.Remove(projectile);
                 }
             }
-
+            foreach (Projectile projectile in hitProjectiles)
+                activeProjectiles.Remove(projectile);
+            hitProjectiles = new LinkedList<Projectile>();
             // Postpone the ring closing for the low target, right now functional minimum!!
             /*
             //close the ring of doom
@@ -186,7 +209,27 @@ namespace GameLab
                 mesh.Draw();
             }
         }
-
+        private void DrawHealthAndStamina()
+        {
+            int n = players.Length;
+            switch (n)
+            {
+                case 4:
+                    _spriteBatch.DrawString(font, "Lives: " + players[3].Life + "  Stamina: " + (int)players[3].Stamina, new Vector2(1500, 950), Color.Yellow);
+                    goto case 3;
+                case 3:
+                    _spriteBatch.DrawString(font, "Lives: " + players[2].Life + "  Stamina: " + (int)players[2].Stamina, new Vector2(10, 950), Color.Orange);
+                    goto case 2;
+                case 2:
+                    _spriteBatch.DrawString(font, "Lives: " + players[1].Life + "  Stamina: " + (int)players[1].Stamina, new Vector2(1500, 10), Color.White);
+                    goto case 1;
+                case 1: 
+                    _spriteBatch.DrawString(font, "Lives: " + players[0].Life + "  Stamina: " + (int)players[0].Stamina, new Vector2(10, 10), Color.Red);
+                    goto default;
+                default:
+                break;
+            }
+        }
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.DeepSkyBlue); // Background color
@@ -216,14 +259,10 @@ namespace GameLab
 
             // Draw all players:
             foreach (Player player in players)
-                DrawModel(playerModel, Matrix.CreateTranslation(player.Position) * playerTranslation * playerScaling);
-
-            // Debug Code:
-            // foreach (Player player in players)
-            //     Console.WriteLine(player.Position);
-
-            // Draw the player's life:
-            _spriteBatch.DrawString(font, "Lives: " + players[0].Life, new Vector2(5, 5), Color.White);
+                DrawModel(playerModel, Matrix.CreateRotationY((float)Math.Atan2(-1f*player.Orientation.X,-1f*player.Orientation.Z))* Matrix.CreateTranslation(player.Position) * playerTranslation);
+            //foreach (Player player in players)  
+            //   Console.WriteLine(player.Position);
+            DrawHealthAndStamina();
             _spriteBatch.End();
 
             OrientedBoundingBox obb1 = OrientedBoundingBox.ComputeOBB(arena.Meshes[15], arenaScaling);
--- src/GameObjects/Player.cs
+++ src/GameObjects/Player.cs
@@ -1,9 +1,9 @@
 using GameLab;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Input;
 using System.Runtime.CompilerServices;
 using System;
 using System.Collections.Generic;
+using Accord.Math.Distances;
 
 namespace src.GameObjects
 {
@@ -13,101 +13,98 @@ namespace src.GameObjects
         // Private fields:
         private float playerSpeed = 2f;
         public int Life { get; set; } = 3;
-        private int stamina = 3;
-        //private Projectile *projectileHeld;
-        private ProjectileType typeOfProjectileHeld = ProjectileType.None;
-        private bool isDashing = false;
+        public float Stamina { get; set; } = 0f;
+        private Projectile projectileHeld;
+        //private ProjectileType typeOfProjectileHeld = ProjectileType.None;
+        private float dashTime = 0f;
         private const float TIME_CATCH_THROW = 0.5f;
-        private float timeSinceCatch = 0f;
+        private float timeSinceThrow = 0f;
+        private bool actionPushed;
+
+        private Input input;
 
         // Constructor: Only allow to assign position here, lifes stamina and so on are a global property and need to be the same for
-        public Player(Vector3 position)
+        public Player(Vector3 position, Input input)
         {
             Position = position;
+            this.input = input;
+            projectileHeld = null;
         }
 
         // The player move method:
         public void Move(float dt)
         {
-            Vector3 dir = new Vector3(0, 0, 0);
-            KeyboardState newState = Keyboard.GetState();
-            if (newState.IsKeyDown(Keys.A))
-                dir.X -= 1;
-
-            if (newState.IsKeyDown(Keys.D))
-                dir.X += 1;
-
-            if (newState.IsKeyDown(Keys.W))
-                dir.Z -= 1;
-
-            if (newState.IsKeyDown(Keys.S))
-                dir.Z += 1;
-
+            Vector3 dir = input.Direction();
             if (dir.Length() > 0)
             {
                 dir = Vector3.Normalize(dir);
                 Orientation = dir;
             }
-
             Position += playerSpeed * dir * dt;
-
-            timeSinceCatch += dt;
         }
 
         // Method to grab an object:
-        public bool Grab(Projectile projectile, LinkedList<Projectile> activeProjectiles)
-        {
-            if (Keyboard.GetState().IsKeyDown(Keys.E)
-                && typeOfProjectileHeld == ProjectileType.None
-                && Vector3.Distance(Position, projectile.Position) < 2.0f
-                && timeSinceCatch > TIME_CATCH_THROW)
-            {
-                this.typeOfProjectileHeld = projectile.Type;
-                activeProjectiles.Remove(projectile);
-                timeSinceCatch = 0f;
-                Console.WriteLine("Grabbing " + projectile.Type);
-                return true;
+        public bool GrabOrHit(Projectile projectile, LinkedList<Projectile> activeProjectiles)
+        {   
+            if(projectile != projectileHeld){
+                if (input.Action() 
+                    && projectileHeld == null
+                    && Vector3.Distance(Position, projectile.Position) < 1.0f
+                    && timeSinceThrow>1f)
+                {
+                    projectileHeld = projectile;
+                    projectile.Caught(this);
+                    Console.WriteLine("Grabbing " + projectile.Type);
+                    return false;
+                }else if (Vector3.Distance(Position, projectile.Position) < 0.5)
+                {
+                    Life--;
+                    return true;
+                }
             }
-
             return false;
         }
 
         // Method to throw an object:
-        public void Throw(LinkedList<Projectile> activeProjectiles)
+        public void Throw(float dt)
         {
-            if (Keyboard.GetState().IsKeyDown(Keys.E)
-                && typeOfProjectileHeld != ProjectileType.None
-                && timeSinceCatch > TIME_CATCH_THROW)
+            if (input.Action() && !actionPushed)
             {
-                activeProjectiles.AddLast(Projectile.createProjectile(typeOfProjectileHeld, Position + Orientation, Orientation));
-                typeOfProjectileHeld = ProjectileType.None;
-                timeSinceCatch = 0f;
-
+                projectileHeld.Throw();
+                projectileHeld = null;
+                timeSinceThrow = 0f;
                 Console.WriteLine("Throwing projectile with orientation: " + Orientation);
             }
         }
 
-
-        public bool GetHit(Projectile projectile, LinkedList<Projectile> activeProjectiles, Player[] Players)
+        // Method to dash:
+        public bool Dash(float dt)
         {
-            // Very basic check, DOES NOT USE HITBOXES YET!
-            if (Vector3.Distance(Position, projectile.Position) < 0.5f)
-            {
-                Life--;
-                if(projectile.Type == ProjectileType.Frog) {
-                    Projectile.frogCount--;
-                }
-                activeProjectiles.Remove(projectile);
-                return true;
+            if(input.Dash() &&dashTime<=0f && Stamina>60f){
+                dashTime=0.1f;
+                Stamina-=60f;
             }
-
-            return false;
+            if(dashTime>0f){
+                Position += 6*playerSpeed * Orientation * dt;
+                dashTime -= dt;
+                return true;
+            }else{
+                return false;
+            }
         }
 
-        // Method to dash:
-        public void Dash()
-        {
-            this.isDashing = true;
+        public void update(float dt){
+            Stamina +=dt*10;
+            Stamina = (Stamina >100f) ? 100f : Stamina;
+            if(!Dash(dt)){
+                Move(dt);
+                if(projectileHeld != null){
+                    Throw(dt);
+                }else {
+                    timeSinceThrow += dt;
+                }
+            }
+            actionPushed = input.Action();
         }
     }
 }
